d1df76f2e9ea43ecfd7873d0f472e05944f5ac09
pranavz <mailz4pranav@gmail.com>
2021-12-15 11:40:52 +0530

    to do almost done without delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,78 @@
-import './App.css'
-import React, {useState} from 'react'
-import Header from './components/Header'
-import Counter from './Counter'
+import React from 'react';
+import './App.css';
+
+import { useState } from 'react'
 
 
-// import Counter from './Counter'
 function App() {
 
-  const [state,setState] = useState(false)
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
 
   return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
 
-  <div>
 
-<h1 onClick={()=>setState(!state)} >  Show and hide </h1>
+      { toDos.map((obj) => {
 
-{ state? <Counter/>:null}
 
-</div>
-  
+        return (
+
+          <div className="todos">
+          <div className="todo">
+            <div className="left">
+              <input  onChange={(e)=>{setToDos(toDos.filter(obj2=>{
+
+                if(obj2.id===obj.id){
+
+                    obj2.status=e.target.checked 
+
+                }
+                return obj2
+
+              }))}}   value={obj.status} type="checkbox" name="" id="" />
+
+              <p>{obj.text}</p>
+          
+              </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        </div>
+
+        )
+      })}
+
+
+
+
+  {toDos.map((obj)=>{
+
+  if(obj.status){
+
+    return(
+
+      <h1>{obj.text}</h1>
+
+    )
+
+  }
+
+  return null
+  })}
+    </div>
   );
 }
-
 export default App;
-
